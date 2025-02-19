5ffea1eab6fbe6de72e7113475498951a25a3100
izhannafees2 <izhannafees5@gmail.com>
2025-02-19 10:32:30 +0500

    Assignment-5

---
+++ assignment5.js
@@ -0,0 +1,209 @@
+// Ans no 1
+// let userFristName = prompt ('Enter your first name')
+// let userLastName = prompt ('Enter your last name')
+
+// document.write('Hello' + userFristName + userLastName)
+
+// Ans no 2
+// let userMobileModel = prompt ('Enter your mobile model')
+// document.write('your mobile model length is' + userMobileModel.length)
+
+// Ans no 3
+// var countryName = 'pkaistan'
+// document.write(countryName.indexOf('k'))
+
+//  Ans no 5
+//  var countryName = 'pkaistan'
+//  document.write(countryName.charAt('3'))
+
+// Ans no 7
+// var cityName = 'heydarabad'
+// var newCity = cityName.replace("heydar","islam")
+
+// document.write("<h2>"+ cityName + "</h2>")
+// document.write("<h2>"+ newCity + "</h2>")
+
+// Ans no 8
+//  var cityName = 'Ali and Sami are best friends. They play cricket and football together.'
+//  var newCity = cityName.replace(/and/g,"&")
+
+//  document.write("<h2>"+ cityName + "</h2>")
+//  document.write("<h2>"+ newCity + "</h2>")
+
+// Ans no 9 
+// var str = "472"
+// var num = 472
+// document.write("472" + typeof str + "<br>" + "427" + typeof num)
+
+// Ans no 10
+
+// let URL = prompt ("Enter your URL");
+// var Domain = URL.split("www.", "");
+
+// document.write( URL)
+// document.write(Domain);
+
+// // User se URL input lena
+// var url = prompt("Enter a URL (e.g., www.facebook.com, www.yahoo.com):");
+
+// // "www." ko replace karna
+// var domain = url.replace("www.", "");
+
+// // Browser par result show karna
+// document.write("<h2>Entered URL: " + url + "</h2>");
+// document.write("<h3>Domain Name: " + domain + "</h3>");
+
+// var userInput = prompt("Enter a word:");
+
+// var upperCaseInput = userInput.toUpperCase();
+
+// document.write("User input:", userInput);
+// document.write("Upper case:", upperCaseInput);
+
+
+
+// var userInput = prompt("Enter a word:");
+// var lowerCaseInput = userInput.toLowerCase();
+
+
+//  document.write("Lower case: " + lowerCaseInput);
+
+
+
+// var num = 35.36;
+// var strNum = num.toString().replace(".", "");
+// document.write("Number:", num);
+// document.write("Result:", strNum);
+
+// var a = "3";
+// var b = "3";
+// var x = a + b; 
+// document.write("a is", a, "<br>");
+// document.write("b is", b, "<br>");
+// document.write("a + b is", x, "<br>");
+
+
+// var a = "3";
+// var b = "3";
+// var x = a - b; 
+// document.write("a is", a, "<br>");
+// document.write("b is", b, "<br>");
+// document.write("a - b is", x, "<br>");
+
+// var university = "University of Karachi";
+// var charArray = university.split(""); 
+
+// for (var i = 0; i < charArray.length; i++) {
+//     document.write(charArray[i] + "<br>"); 
+// }
+
+// var userInput = prompt("Enter a word:");
+// var lastChar = userInput.charAt(userInput.length - 1); 
+
+// document.write("User input: " + userInput + "<br>");
+// document.write("Last character of input: " + lastChar);
+
+
+
+
+// var str =prompt("Enter any word")
+// str = str.toLowerCase(); 
+
+// var vowels = "aeiou";
+// var vowelCount = 0;
+// var consonantCount = 0;
+
+// for (var i = 0; i < str.length; i++) {
+//     if (vowels.includes(str[i])) {
+//         vowelCount++;
+//     } else if (str[i] >= 'a' && str[i] <= 'z') { 
+//         consonantCount++;
+//     }
+// }
+
+// document.write("String: " + str + "<br>");
+// document.write("Number of vowels: " + vowelCount + "<br>");
+// document.write("Number of consonants: " + consonantCount);
+
+
+
+
+
+
+// var userInput = prompt("Enter a word:");
+
+// var upperCaseInput = userInput.toUpperCase();
+
+// document.write("User input:", userInput);
+// document.write("Upper case:", upperCaseInput);
+
+
+
+// var userInput = prompt("Enter a word:");
+// var lowerCaseInput = userInput.toLowerCase();
+
+
+//  document.write("Lower case: " + lowerCaseInput);
+
+
+
+// var num = 35.36;
+// var strNum = num.toString().replace(".", "");
+// document.write("Number:", num);
+// document.write("Result:", strNum);
+
+// var a = "3";
+// var b = "3";
+// var x = a + b; 
+// document.write("a is", a, "<br>");
+// document.write("b is", b, "<br>");
+// document.write("a + b is", x, "<br>");
+
+
+// var a = "3";
+// var b = "3";
+// var x = a - b; 
+// document.write("a is", a, "<br>");
+// document.write("b is", b, "<br>");
+// document.write("a - b is", x, "<br>");
+
+// var university = "University of Karachi";
+// var charArray = university.split(""); 
+
+// for (var i = 0; i < charArray.length; i++) {
+//     document.write(charArray[i] + "<br>"); 
+// }
+
+// var userInput = prompt("Enter a word:");
+// var lastChar = userInput.charAt(userInput.length - 1); 
+
+// document.write("User input: " + userInput + "<br>");
+// document.write("Last character of input: " + lastChar);
+
+
+
+
+// var str =prompt("Enter any word")
+// str = str.toLowerCase(); 
+
+// var vowels = "aeiou";
+// var vowelCount = 0;
+// var consonantCount = 0;
+
+// for (var i = 0; i < str.length; i++) {
+//     if (vowels.includes(str[i])) {
+//         vowelCount++;
+//     } else if (str[i] >= 'a' && str[i] <= 'z') { 
+//         consonantCount++;
+//     }
+// }
+
+// document.write("String: " + str + "<br>");
+// document.write("Number of vowels: " + vowelCount + "<br>");
+// document.write("Number of consonants: " + consonantCount);
+
+
+
+
+
+
